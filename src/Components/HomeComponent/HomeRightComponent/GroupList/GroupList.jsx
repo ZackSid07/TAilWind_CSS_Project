f31aca3c1bf047fd933d5b59c5ffc0c4ecd0747e
ZackSid07 <algalib908@gmail.com>
2024-10-18 02:04:54 +0600

    Your commit message

--- src/Components/HomeComponent/HomeRightComponent/GroupList/GroupList.jsx
+++ src/Components/HomeComponent/HomeRightComponent/GroupList/GroupList.jsx
@@ -6,6 +6,15 @@ import avater from "../../../../assets/Home/HomeRight/Cat.gif"
 import Cropper from "react-cropper";
 import Modal from 'react-modal';
 import "cropperjs/dist/cropper.css";
+import { ErrorToast, SuccessToast } from '../../../../../Utils/ToastMessage/Toast.js'
+import { getStorage, ref, uploadString, getDownloadURL } from "firebase/storage";
+import { v4 as uuidv4 } from 'uuid';
+import { data } from 'autoprefixer'
+import { getDatabase, ref as dbref, set, push } from "firebase/database";
+import { getAuth } from 'firebase/auth' 
+import moment from 'moment';
+
+
 const defaultSrc =
   "https://raw.githubusercontent.com/roadmanfong/react-cropper/master/example/img/child.jpg";
 const customStyles = {
@@ -23,6 +32,16 @@ const customStyles = {
 const GroupList = () => {
 
   const [modalIsOpen, setIsOpen] = useState(false);
+  const [groupInputValue , setgroupInputValue] = useState({
+    groupName:"",
+    GrouptagName:"",
+  
+  })
+  const [loading , setloading] = useState(false)
+  const storage = getStorage();
+  const db = getDatabase();
+  const auth = getAuth();
+
 
   function openModal() {
     setIsOpen(true);
@@ -56,16 +75,76 @@ const GroupList = () => {
   const getCropData = () => {
     if (typeof cropperRef.current?.cropper !== "undefined") {
       setCropData(cropperRef.current?.cropper.getCroppedCanvas().toDataURL());
+      SuccessToast("Crop Done" , "top-center");
+   
     }
   };
 
-  console.log(cropData);
+//onGroupchangehandler function implement
+const onGroupchangehandler = (event) => {
+  const { id, value } = event.target;
+  setgroupInputValue({
+    ...groupInputValue,
+    [id]: value,
+  });
+};
+
+
+
+
+
+
+
+
+/**
+ * todo: handleGroup function implement
+ */
+
+const handleGroup = ()=>{
+  const {grouptagName , groupName} = groupInputValue;
+  if(!cropData || !grouptagName || !groupName){
+    return ErrorToast(`Must Fillup the grouptagName or the groupName or Cropping Image`, "top-center" , 7000);
+  }
+  setloading(true)
+  // console.log(groupInputValue.cropimage);
+  const storageRef = ref(storage, `GroupImage/image${uuidv4() }`);
+  uploadString(storageRef,cropData , 'data_url')
+  .then((snapshot) => {
+    // console.log('Uploaded a data_url string!', snapshot);
+    setCropData("");
+     
+  }).then(()=>{
+    return getDownloadURL(storageRef);
+  }).then((groupDownloadUrl)=>{
+    set(push(dbref(db, 'groups/')), {
+      whoCreatedGroup: auth.currentUser.uid,
+      whoCreatedGroupName: auth.currentUser.displayName,
+      whoCreatedGroupProfile_picture: auth.currentUser.photoURL,
+      whoCreatedGroupEmail: auth.currentUser.email,
+      groupName: groupInputValue.groupName,
+      GroupTagName: groupInputValue.GrouptagName,
+      groupPhoto: groupDownloadUrl,
+      createdAt:moment().format("MM DD YYYY, h:mm:ss a"),
+    });
+    
+  }).catch((error)=>{
+    setloading(false)
+    ErrorToast(`Error form Create Group$`, "top-center")
+  })
   
-
-
-
-
-
+  
+  .finally(()=>{
+    setgroupInputValue({
+      groupName:"",
+      GrouptagName:""
+    
+    });
+    setCropData(""); 
+    setImage("");
+    setloading(false);
+    closeModal();
+  })
+}
 
 
   
@@ -124,10 +203,23 @@ const GroupList = () => {
         <h1 className='capitalize'>Group Information</h1>
         <form on onSubmit={(e)=>e.preventDefault()}>
           <label htmlFor="groupName">{" "}GroupName <span className='text-red-500'>*</span></label>
-          <input type='text' className='w-full bg-gray-300 border-2 border-red-200 py-2 px-2 rounded-md' id='groupName' name='groupName' value={""} />
+          <input
+           type='text' 
+           className='w-full bg-gray-300 border-2 border-red-200 py-2 px-2 rounded-md'
+            id='groupName'
+            name='groupName'
+            value={groupInputValue.groupName} 
+            onChange={onGroupchangehandler}
+          />
 
           <label htmlFor="GrouptagName">{" "}GrouptagName <span className='text-red-500'>*</span></label>
-          <input type='text' className='w-full bg-gray-300 border-2 border-red-200 py-2 px-2 rounded-md' id='GrouptagName' name='GrouptagName' value={""} />
+          <input type='text'
+           className='w-full bg-gray-300 border-2 border-red-200 py-2 px-2 rounded-md'
+           id='GrouptagName' 
+           name='GrouptagName' 
+           value={groupInputValue.GrouptagName}
+           onChange={onGroupchangehandler}
+          />
 
           {/* Cropper Body */}
         <div>
@@ -157,7 +249,7 @@ const GroupList = () => {
           </div>
             {/* Preview */}
             <div className=" bg-red-500 h-[250px] w-[350px] relative">
-              <h1 className='absolute -top-7 '>Preview</h1>
+              <h1 className='absolute -top-[63px] px-5 py-3 rounded-xl text-white bg-red-600'>Preview</h1>
               <div className="img-preview overflow-hidden h-[250px] z-10 preview_box"/>
             </div>
            {/* Preview */}
@@ -167,7 +259,7 @@ const GroupList = () => {
             <div className=" bg-blue-500 h-[250px] w-[350px] relative">
               <h1 className='absolute -top-7'>
                 
-                <button style={{ float: "right" }} onClick={getCropData} className='bg-red-500'>
+                <button style={{ float: "right" }} onClick={getCropData} className='absolute -top-[33px] px-5 py-3 rounded-xl text-white bg-blue-500 text-nowrap'>
                   Crop Image
                 </button>
               </h1>
@@ -181,8 +273,11 @@ const GroupList = () => {
         </div>
           {/* Cropper Body */}
 
+          {loading ? (<button className='w-full py-3 bg-blue-600 mt-10 text-white text-base rounded-xl'
+           >Loading........</button>) : (<button className='w-full py-3 bg-blue-600 mt-10 text-white text-base rounded-xl'
+            onClick={handleGroup}
+            >Create Groups</button>)}
 
-          <button className=''>Create Groups</button>
           
         </form>
       </Modal>
